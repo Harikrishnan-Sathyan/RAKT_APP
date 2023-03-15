c8ea52bbf4f6d99983ea304a1e21b766ef2c1cf8
Azriel <azrielmen23@gmail.com>
2023-03-15 11:22:47 +0530

    azzi init

---
+++ app/src/main/java/com/example/raktdaan/Login.java
@@ -0,0 +1,29 @@
+package com.example.raktdaan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+    TextView regist;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        regist = findViewById(R.id.register);
+
+        regist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Login.this,Register.class);
+                startActivity(i);
+                Toast.makeText(Login.this, "Loading...", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
