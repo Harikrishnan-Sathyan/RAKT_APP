25a1b9df0dcaa60308814415961ea6f14223513c
Harikrishnan-Sathyan <harikrishnan.kongiparambil023@nmims.edu.in>
2023-04-10 11:55:24 +0530

    added register butoon

--- app/src/main/java/com/example/raktdaan/Register.java
+++ app/src/main/java/com/example/raktdaan/Register.java
@@ -3,8 +3,12 @@ package com.example.raktdaan;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class Register extends AppCompatActivity {
+    Button register_btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +16,13 @@ public class Register extends AppCompatActivity {
 
         setContentView(R.layout.activity_register);
 
+        register_btn = findViewById(R.id.button2);
+
+        register_btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(Register.this, "Registering", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
